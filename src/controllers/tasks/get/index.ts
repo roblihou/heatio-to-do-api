c43a77f2a88b40859c7ae6a07faa368e71e174ee
Rob Lihou <rob.lihou@tred.earth>
2025-02-04 13:13:14 +0000

    simulate slow api

--- src/controllers/tasks/get/index.ts
+++ src/controllers/tasks/get/index.ts
@@ -2,8 +2,10 @@ import { Request, Response } from "express";
 import { handleAPIError } from "@utils/handleAPIError";
 import Store from "@services/Store";
 import { HttpStatusCode } from "@constants/http";
+import sleep from "@utils/sleep";
 
 const getTasks = async (req: Request, res: Response) => {
+  await sleep(1000); // Simulate a slow API response
   try {
     const { tasks } = Store;
 
--- src/utils/handleAPIError/index.ts
+++ src/utils/handleAPIError/index.ts
@@ -19,15 +19,6 @@ export const handleAPIError = (
 
   const errors = parseYupError(err);
 
-  if (errors.some((error) => error.path?.split(".")[0] === "user")) {
-    console.error(
-      "Authentication middleware failed to correctly assign the user object to the request object"
-    );
-    res.status(HttpStatusCode.UNAUTHORIZED);
-    res.send();
-    return;
-  }
-
   res.status(HttpStatusCode.BAD_REQUEST);
   res.send({
     errors,
